b1b40bb862c752a065af5c019a616d79caf81258
Omar Ahmed <49450963+Nrody@users.noreply.github.com>
2022-02-07 18:01:18 +0000

    constructor

--- src/main/java/fsad/week2/todoapplication/dataModel/toDoItem.java
+++ src/main/java/fsad/week2/todoapplication/dataModel/toDoItem.java
@@ -1,7 +1,16 @@
 package fsad.week2.todoapplication.dataModel;
 
+import java.time.LocalDate;
+
 public class toDoItem {
     private String shortDescription;
-    private String;
+    private String details;
+    private LocalDate deadline;
+
+    public toDoItem(String shortDescription, String details, LocalDate deadline) {
+        this.shortDescription = shortDescription;
+        this.details = details;
+        this.deadline = deadline;
+    }
 
 }
