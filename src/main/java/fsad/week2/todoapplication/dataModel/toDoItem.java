71b6db64eb18e8f47f13635709a9c92746e60db4
Omar Ahmed <49450963+Nrody@users.noreply.github.com>
2022-02-07 18:05:38 +0000

    getters/setters

--- src/main/java/fsad/week2/todoapplication/dataModel/toDoItem.java
+++ src/main/java/fsad/week2/todoapplication/dataModel/toDoItem.java
@@ -13,4 +13,24 @@ public class toDoItem {
         this.deadline = deadline;
     }
 
+    public String getShortDescription() {
+        return shortDescription;
+    }
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
 }
